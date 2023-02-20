0975cc1c23f5602e811e5c6f57a718143334b2b9
fdd4s <114248002+fdd4s@users.noreply.github.com>
2023-02-20 05:08:12 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# floorfy-dl
-Download floorfy virtual tours to equirectangular and skybox
+# floorfy downloader
+
+## What does
+
+floorfy-dl downloads cubemap skybox 360 and equirectangular panos photos of floorfy houses virtual tours 
+
+## Dependencies
+
+php, php-curl, curl  
+
+This code can run over Linux and Windows  
+
+## Usage
+
+    $ php ./floorfy-dl.php <floorfy url>  
+
+e.g: If the url is https://floorfy.com/tour/417174 you have to run the script this way:  
+
+    $ php ./floorfy-dl.php https://floorfy.com/tour/417174  
+
+It will download all the skybox and equirectangular images.
+
+## Viewers
+
+Web Browser: Chrome and Firefox: Pannellum (webgl based), self hosted virtual tour, equirectangular and cubemap skybox: https://github.com/mpetroff/pannellum
+
+Android: (only equirectangular) Ricoh Theta App https://play.google.com/store/apps/details?id=com.theta360&hl=en&gl=US  
+
+PC: Linux/Mac/Windows Panini and opening as Cube Faces or Equirectangular: https://github.com/lazarus-pkgs/panini  
+
+## Equirectangular
+
+Floorfy stores panos in Skybox and Equirectangular format, but the skybox version of floorfy has higher quality, to keep high quality in equirectangular, skybox can be converted to equirectangular with this tool:
+
+https://github.com/fdd4s/skybox2equirectangular  
+
+Equirectangular can be viewed in players like Ricoh Theta for Android https://play.google.com/store/apps/details?id=com.theta360&hl=en&gl=US  
+
+## Credits
+
+Created by fdd  
+Send feedback and questions to fdd4776s@gmail.com  
+Support future improvements of this software https://www.buymeacoffee.com/fdd4s  
+All files are public domain https://unlicense.org/  
---
+++ floorfy-dl.php
@@ -0,0 +1,273 @@
+<?PHP
+
+if (count($argv)!=2)  { echo ("usage: php ./floorfy-dl.php <url floorfy virtual tour>\n"); exit(0); }
+$url = $argv[1];
+
+$floorfy = new Floorfy($url);
+
+$floorfy->download();
+
+class Floorfy {
+	var $id;
+	var $url;
+	var $validId;
+	var $http_referer;
+	var $baseUrl;
+	var $urlList;
+
+	public function download() {
+		if ($this->validId==false) return;
+		echo("url ".$this->url."\n");
+		$urlHtml = new Url($this->url, "", "");
+		$urlHtml->download();
+		$urlJson = new Url($this->getUrlJson($urlHtml->data), "", "");
+		$urlJson->download();
+		$this->parseConfigJson($urlJson->data);
+	}
+
+	public function getUrlJson($data) {
+		$urlJson = "https://cdn.floorfy.com/".$this->strBtn($data, '<input id="configpath" type="hidden" value="/', '"');
+		$this->baseUrl = str_replace("config.json", "", $urlJson);
+		return $urlJson;
+	}
+
+	private function strBtn($str_content, $str_start, $str_end) {
+		$pos1 = strpos($str_content, $str_start);
+		if ($pos1===FALSE) return "";
+		$pos1 = $pos1 + strlen($str_start);
+
+		$pos2 = strpos($str_content, $str_end, $pos1);
+		if ($pos2===FALSE) return "";
+
+		$res_len = $pos2 - $pos1;
+		$res = substr($str_content, $pos1, $res_len);
+
+		return $res;
+	}
+
+	public function parseConfigJson($data) {
+		file_put_contents(dirname(__FILE__)."/cfg_".$this->id.".json", $data);
+
+		$json_arr = json_decode($data, true);
+		$ids = array();
+
+		foreach($json_arr['scenes'] as $scene) {
+			$ids[] = $scene['id'];
+		}
+
+		$this->makeUrlList($ids);
+	}
+
+	public function makeUrlList($ids) {
+		$cube = "f.b.l.r.u.d";
+		$cubeFaces = explode(".", $cube);
+		foreach($ids as $id) {
+			$url = $this->baseUrl."4k_".$id.".jpg";
+			$name = "equi_".str_replace("equirectangular_", "", $id).".jpg";
+
+			$this->urlList->add($url, $name, "e");
+
+			foreach($cubeFaces as $cubeFace) {
+				$url = $this->baseUrl.$id."/fallback/".$cubeFace.".jpg";
+				$name = "skybox_".str_replace("equirectangular_", "", $id)."_".$cubeFace.".jpg";
+
+				$this->urlList->add($url, $name, "s");
+			}
+		}
+		$this->urlList->debug();
+		$this->urlList->download();
+	}
+
+	public function __construct($url) {
+		$this->urlList = new UrlList();
+		$this->getIdFromUrl($url);
+		if ($this->validId==false) return;
+		$this->url = "https://floorfy.com/tour/".$this->id;
+	}
+	
+	private function getIdFromUrl($url) {
+		$this->valid_id = false;
+		if (strpos($url, "floorfy.com")===false) return;
+		if (strpos($url, "tour/")===false) return;
+		$arr = explode("tour/", $url);
+		$arr2 = explode("/", $arr[1]);
+		$this->id = $arr2[0];
+		if (strlen($this->id)>0) $this->validId = true;
+	}
+	
+	public function debug() {
+		echo("floorfly->validId	= ".$this->valid_id.";\n");
+		echo("floorfly->id	= ".$this->id.";\n");
+		echo("floorfly->url	= ".$this->url.";\n");
+	}
+}
+
+class UrlList {
+	var $arr;
+
+	public function __construct() {
+		$this->arr = array();
+	}
+
+	public function add($url, $name, $type) {
+		$url = new Url($url, $name, $type);
+		$this->arr[] = $url; 
+	}
+
+	public function download() {
+		foreach($this->arr as $url) {
+			$url->download();
+		}
+	}
+
+	public function debug() {
+		foreach ($this->arr as $url) {
+			$url->debug();
+		}
+	}
+}
+
+class Url {
+	const HTTP_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
+	const HTTP_ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
+	const HTTP_ACCEPT_LANGUAGE = "en-US,en;q=0.5";
+	
+	var $url;
+	var $path;
+	var $type;
+	var $name;
+	var $isOk;
+	var $isData;
+	var $data;
+
+	public function __construct($url, $name, $type) {
+		$this->isOk = false;
+		$this->url = $url;
+		$this->type = $type;
+		if (strlen($name)==0) {
+			$this->isOk = true;
+			$this->isData = true;
+		} else {
+			$this->setName($name);
+			$this->isData = false;
+		}
+	}
+	
+	public function debug() {
+		echo("url->url = ".$this->url.";\n");
+		//echo("url->path = ".$this->path.";\n");
+		//echo("url->name = ".$this->name.";\n");
+		//echo("url->type = ".$this->type.";\n");
+	}
+
+	public function setName($name) {
+		$name = $this->formatFilename($name);
+		if (strlen(str_replace(".", "", $name))<1) return;
+		$this->name = $name;
+		$this->path = dirname(__FILE__)."/".$name;
+		$this->isOk = true;
+	}
+
+	private function formatFilename($cad) {
+		$cad = str_replace("/", "_", $cad);
+		$cad = str_replace("-", "_", $cad);
+		$imax = strlen($cad);
+		$char_val = " ";
+		$char_num = 0;
+		$char_valid = false;
+
+		$res = "";
+		for ($i=0; $i<$imax; $i++) {
+			$char_val = substr($cad, $i, 1);
+			$char_num = ord($char_val);
+			$char_valid = false;
+
+			if ($char_num >= ord("0") && $char_num <= ord("9")) {
+				$char_valid = true;
+			}
+
+
+			if ($char_num >= ord("a") && $char_num <= ord("z")) {
+				$char_valid = true;
+			}
+
+
+			if ($char_num >= ord("A") && $char_num <= ord("Z")) {
+				$char_valid = true;
+			}
+
+			if ($char_val==".") $char_valid = true;
+			if ($char_val=="_") $char_valid = true;
+			if ($char_val=="(") $char_valid = true;
+			if ($char_val==")") $char_valid = true;
+
+			if ($char_valid==true) {
+				$res .= $char_val;
+			}
+		}
+		return $res;
+	}
+
+	public function download() {
+		echo("Downloading ".$this->url."\n");
+
+		if ($this->isData==true) {
+			$this->data = $this->httpStr($this->url);
+		} else {
+			if (file_exists($this->path)) return true;
+			$this->httpPath($this->url, $this->path);
+		}
+		return true;
+	}
+
+	private function httpStr($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$headers = array();
+		$headers[] = "Accept: ".self::HTTP_ACCEPT;
+		$headers[] = "Accept-Language: ".self::HTTP_ACCEPT_LANGUAGE;
+
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_USERAGENT, self::HTTP_USER_AGENT);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+
+		$output = curl_exec($ch);  
+
+		curl_close($ch); 
+
+		return $output;
+	}
+	
+	private function httpPath($url, $path) {
+		$fp = fopen($path, 'w+');
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$headers = array();
+		//$headers[] = "Referer: ".$this->http_referer;
+		$headers[] = "Accept: ".self::HTTP_ACCEPT;
+		$headers[] = "Accept-Language: ".self::HTTP_ACCEPT_LANGUAGE;
+
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_USERAGENT, self::HTTP_USER_AGENT);
+		curl_setopt($ch, CURLOPT_FILE, $fp); 
+		curl_exec($ch);
+
+		$res = false;
+		if(curl_errno($ch)==0) $res = true; else { echo ("error curl ".curl_errno($ch)."\n"); }
+
+		curl_close($ch); 
+		fclose($fp);
+
+		return $res;
+	}
+}
+
+?>
